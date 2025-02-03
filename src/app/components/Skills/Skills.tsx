5665f91feb53fec50e4254b5dd14c647359c9e97
Andreseag <castrogomezand@gmail.com>
2025-02-03 14:59:27 -0500

    feat: fix errors in card skill :sparkles:

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -1,18 +1,77 @@
-import React from "react";
+const developerSkills = [
+  {
+    name: "JavaScript",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    name: "TypeScript",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+  },
+  {
+    name: "React",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+  },
+  {
+    name: "Next.js",
+    image:
+      "https://images.seeklogo.com/logo-png/32/1/next-js-logo-png_seeklogo-321806.png",
+  },
+  {
+    name: "Node.js",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+  },
+  {
+    name: "Python",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+  },
+  {
+    name: "HTML",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+  },
+  {
+    name: "CSS",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+  },
+  {
+    name: "Git",
+    image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg",
+  },
+  {
+    name: "Docker",
+    image:
+      "https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png",
+  },
+  {
+    name: "MongoDB",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg",
+  },
 
-const skillsData = [
-  "Typescript",
-  "Javascript",
-  "ReactJS",
-  "VueJS",
-  "Nuxt",
-  "Nextjs",
-  "HTML5",
-  "CSS3",
-  "SASS",
-  "Tailwind",
-  "Bootstrap",
-  "Figma",
+  {
+    name: "Vue",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg",
+  },
+  {
+    name: "Nuxt",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Nuxt_logo.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+  },
+  {
+    name: "Sass",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg",
+  },
 ];
 
 const Skills = () => {
@@ -24,12 +83,12 @@ const Skills = () => {
           <p>Tecnologías y herramientas que manejo en mi día a día</p>
         </div>
         <div className="skills__items grid grid-cols-6 gap-3">
-          {skillsData.map((skill) => (
+          {developerSkills.map((skill) => (
             <div
-              key={skill}
-              className="skills__item bg-gray-1 p-3 rounded-lg mb-3"
+              key={skill.name}
+              className="skills__item bg-gray-1 p-3 h-28 rounded-lg mb-3 flex items-center justify-center"
             >
-              {skill}
+              <img src={skill.image} alt={skill.name} width={60} height={60} />
             </div>
           ))}
         </div>
